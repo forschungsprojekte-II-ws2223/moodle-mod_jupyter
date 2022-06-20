b54aa9379864b25d00d41530f469665fed4be225
Maximilian Schüle <max.schuele@gmail.com>
2022-06-20 23:52:59 +0200

    working auth (local)

--- jupyter/auth/auth.php
+++ jupyter/auth/auth.php
@@ -1,17 +1,19 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+$jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6InVzZXIiLCJpYXQiOjE1MTYyMzkwMjJ9.khRbDuF1o5ZBSuM94UqI7sS-r6knwoHUDrI6-whE76E';
 $jar = new \GuzzleHttp\Cookie\CookieJar();
 $client = new \GuzzleHttp\Client(
     [
         'cookies' => $jar,
-        'allow_redirects' => false,
-        'headers' => ['Authorization' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6InVzZXIiLCJpYXQiOjE1MTYyMzkwMjJ9.khRbDuF1o5ZBSuM94UqI7sS-r6knwoHUDrI6-whE76E']
+        'allow_redirects' => ['max' => 7],
+        'headers' => ['Authorization' => $jwt]
     ]
 );
 
 $response = $client->request('GET', 'localhost:8000/hub/login?next=%2Fhub%2F');
 
-//echo $response->getHeaderLine('set-cookie'); 
-var_dump($jar)
+//var_dump($response)
+//var_dump($jar)
+echo $response->getBody()
 ?>
