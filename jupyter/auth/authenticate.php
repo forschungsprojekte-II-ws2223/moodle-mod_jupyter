200cfc57ac7db3f33a67c301dcac05a18752ce40
st167110 <st167110@stud.uni-stuttgart.de>
2022-06-21 19:07:09 +0200

    attempt of loading iframe with request

--- jupyter/auth/authenticate.php
+++ jupyter/auth/authenticate.php
@@ -2,75 +2,61 @@
 
 require(__DIR__.'/../../../config.php');
 require_once(__DIR__.'/../lib.php');
-
-//$id = required_param('id', PARAM_INT);
-// $PAGE->set_url('/mod/jupyter/index.php', array('id' => $id));
-
 require (__DIR__ . '/../vendor/autoload.php');
-$id = $_GET['id'];
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+use GuzzleHttp\TransferStats;
 
+$adress = 'host.docker.internal:8000';
+$name = required_param('name', PARAM_INT);
+
+//custom key must equal key in /../../jupyterhub_docker/jupyterhub/jupyterhub_config.py !!!
 $key = 'your-256-bit-secret';
-
 $data = [
-  'id' =>$id
+  'name' =>$name
 ];
 
-// /**
-//  * IMPORTANT:
-//  * You must specify supported algorithms for your application. See
-//  * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
-//  * for a list of spec-compliant algorithms.
-//  */
-$jwt = JWT::encode($data, $key, 'HS256');
 
-
-$testJWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Im1pY2hhZWwgam9uam9uIiwiaWF0IjoxNTE2MjM5MDIyfQ.vgbBQixT0H_LOhFqARyqSPGDjkNoBDTEfDRQ8LhH8Tc';
+//$jwt = JWT::encode($data, $key, 'HS256');
+$jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6InVzZXIiLCJpYXQiOjE1MTYyMzkwMjJ9.khRbDuF1o5ZBSuM94UqI7sS-r6knwoHUDrI6-whE76E';
 print_r("Authorization: ".$jwt);
 
-$url = 'host.docker.internal:8000';
+$jar = new \GuzzleHttp\Cookie\CookieJar();
+$client = new \GuzzleHttp\Client(
+    [
+        'cookies' => $jar,
+        'allow_redirects' => ['max' => 10],
+        'headers' => ['Authorization' => $jwt],
+        'http_errors'=>false
+    ]
+);
 
-///////////////////////////
+$response = $client->request('GET', $adress,[
+  'on_stats' => function (TransferStats $stats) use (&$url) {
+  $url = $stats->getEffectiveUri();
+}]);
 
-// a little script check is the cURL extension loaded or not
-if(!extension_loaded("curl")) {
-  die("cURL extension not loaded! Quit Now.");
-}
+echo $url;
+echo $response->getBody();
 
-// Actual script start
+$templatecontext=[
+  'login'=>$url
+  ];
 
-// create a new cURL resource
-// $curl is the handle of the resource
-$curl = curl_init();
+echo $OUTPUT->render_from_template('mod_jupyter/jupyterhub',$templatecontext);
 
-// set the URL and other options
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: '.$jwt));
-if(curl_error($curl)) {
-  print_r(curl_error($curl));
-}
-// execute and pass the result to browser
-curl_exec($curl);
+// echo $response->getBody();
 
-// close the cURL resource
-curl_close($curl);
-
-
-/////////////////////////////
-
-
-$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
-
-print_r($decoded);
+//$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+//print_r($decoded);
 
 // /*
 //  NOTE: This will now be an object instead of an associative array. To get
 //  an associative array, you will need to cast it as such:
 // */
 
-$decoded_array = (array) $decoded;
+//$decoded_array = (array) $decoded;
 
 //print_r($decoded_array);
 
@@ -81,12 +67,7 @@ $decoded_array = (array) $decoded;
 //  *
 //  * Source: http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#nbfDef
 //  */
-JWT::$leeway = 60; // $leeway in seconds
-$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
 
-$values = array_values($decoded_array);
+// JWT::$leeway = 60; // $leeway in seconds
+// $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
 
-
-  //echo $OUTPUT->header();
-  //echo "<h1>{$values}</h1>";
-  //echo $OUTPUT->footer();
