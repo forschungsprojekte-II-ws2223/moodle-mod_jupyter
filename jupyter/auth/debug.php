6268b41a3a663e038b1b88f4237eac6c94c12571
st167110 <st167110@stud.uni-stuttgart.de>
2022-06-21 13:47:28 +0200

    removed tests

--- jupyter/auth/debug.php
+++ jupyter/auth/debug.php
@@ -2,16 +2,32 @@
 require(__DIR__.'/../../../config.php');
 require_once(__DIR__.'/../lib.php');
 
-require (__DIR__ . '/../vendor/autoload.php');
+require __DIR__ . '/../vendor/autoload.php';
 
-$headers = [
-    "Authorization"=> "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikpva28gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.2c547-cL6FWDfjLCmLp3aQBc7ysdkX7g3lvVL0vZiTc"
-];
 
-$client = new GuzzleHttp\Client();
+$url="https://docs.guzzlephp.org/en/stable/request-options.html#http-errors";
+echo '<iframe src={{$url}} width="70%" height="500" id="iframe" style="border:none;"></iframe>';
 
-$response = $client->get("host.docker.internal:8000", ['headers' =>$headers, 'debug' => true]);
 
-var_dump($response->getStatusCode());
 
-var_dump((string) $response->getBody());
+$jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6InVzZXIiLCJpYXQiOjE1MTYyMzkwMjJ9.khRbDuF1o5ZBSuM94UqI7sS-r6knwoHUDrI6-whE76E';
+$jar = new \GuzzleHttp\Cookie\CookieJar();
+$client = new \GuzzleHttp\Client(
+    [
+        'cookies' => $jar,
+        'allow_redirects' => ['max' => 10],
+        'headers' => ['Authorization' => $jwt],
+        'debug' => false,
+        'http_errors'=>false
+    ]
+);
+var_dump($jar);
+
+$response = $client->request('GET', 'host.docker.internal:8000/hub/login?next=%2Fhub%2F');
+
+//var_dump($response);
+var_dump($jar);
+// $body = $response->getBody();
+// echo $body;
+
+?>
