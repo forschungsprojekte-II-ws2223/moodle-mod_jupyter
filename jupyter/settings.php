821573bf4d23ef022c9031dc3ab22d0139a606d8
Timo Stoll <st117962@stud.uni-stuttgart.de>
2022-07-29 09:14:36 +0200

    Add URL/PORT/IP settings to plugin settings.php

--- jupyter/settings.php
+++ jupyter/settings.php
@@ -29,6 +29,18 @@ if ($hassiteconfig) {
     // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
     if ($ADMIN->fulltree) {
         // TODO: Define the plugin settings page - {@link https://docs.moodle.org/dev/Admin_settings}.
+
+        //Jupyter url setting
+        $settings->add(new admin_setting_configtext('mod_jupyter/jupyter_url', get_string('jupyter_url', 'jupyter'),
+            get_string('jupyter_url_desc', 'jupyter'), 'http://localhost.localdomain/', PARAM_URL));
+
+        //Jupyter IP setting
+        $settings->add(new admin_setting_configtext('mod_jupyter/jupyter_ip', get_string('jupyter_ip', 'jupyter'),
+            get_string('jupyter_ip_desc', 'jupyter'), '255.255.255.255', PARAM_HOST));
+
+        //Jupyter Port setting
+        $settings->add(new admin_setting_configtext('mod_jupyter/jupyter_port', get_string('jupyter_port', 'jupyter'),
+            get_string('jupyter_port_desc', 'jupyter'), 80, PARAM_INT));
+
     }
 }
-
