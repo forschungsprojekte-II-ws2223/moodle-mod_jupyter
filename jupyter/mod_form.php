80f637678ce08b21fe3c8faf818e58b1991a1013
st167110 <st167110@stud.uni-stuttgart.de>
2022-08-31 20:52:52 +0200

    class doc

--- jupyter/mod_form.php
+++ jupyter/mod_form.php
@@ -26,6 +26,13 @@ defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot.'/course/moodleform_mod.php');
 
+/**
+ * Module instance settings form.
+ *
+ * @package     mod_jupyter
+ * @copyright   KIB3 StuPro SS2022 Development Team of the University of Stuttgart
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class mod_jupyter_mod_form extends moodleform_mod {
 
     /**
