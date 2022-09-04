5c31ff6e2152b3336865d97e9c76f1628ebfd5be
Nick Zoller <nick@zoller.name>
2022-09-04 16:07:13 +0200

    added spaces before and after equal signs at variable initializations

--- jupyter/backup/moodle2/backup_jupyter_activity_task.class.php
+++ jupyter/backup/moodle2/backup_jupyter_activity_task.class.php
@@ -53,12 +53,12 @@ class backup_jupyter_activity_task extends backup_activity_task {
         $base = preg_quote($CFG->wwwroot, "/");
 
         // Link to the list of choices
-        $search="/(".$base."\/mod\/jupyter\/index.php\?id\=)([0-9]+)/";
-        $content= preg_replace($search, '$@JUPYTERINDEX*$2@$', $content);
+        $search = "/(".$base."\/mod\/jupyter\/index.php\?id\=)([0-9]+)/";
+        $content = preg_replace($search, '$@JUPYTERINDEX*$2@$', $content);
 
         // Link to choice view by moduleid
-        $search="/(".$base."\/mod\/jupyter\/view.php\?id\=)([0-9]+)/";
-        $content= preg_replace($search, '$@JUPYTERVIEWBYID*$2@$', $content);
+        $search = "/(".$base."\/mod\/jupyter\/view.php\?id\=)([0-9]+)/";
+        $content = preg_replace($search, '$@JUPYTERVIEWBYID*$2@$', $content);
 
         return $content;
     }
