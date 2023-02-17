b06c76896c69e8f55f6dd90dc73b23db98fea47a
Timo Stoll <st117962@stud.uni-stuttgart.de>
2023-02-17 08:43:17 +0000

    Update test

--- tests/git_generator_test.php
+++ tests/git_generator_test.php
@@ -14,27 +14,46 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
+namespace mod_jupyter;
+
 /**
- *
- *
  * @package     mod_jupyter
  * @copyright   KIB3 StuPro SS2022 Development Team of the University of Stuttgart
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-namespace mod_jupyter;
-
 class git_generator_test extends \advanced_testcase {
     public function test_gen_gitfile_standard() {
+        $this->resetAfterTest();
         $gitfilelink = \mod_jupyter\git_generator::gen_gitfilelink("https://github.com/username/reponame", "notebook.ipynb", "branch");
         $this->assertEquals($gitfilelink, "https://github.com/username/reponame/blob/branch/notebook.ipynb");
     }
+
     public function test_gen_gitfile_empty() {
+        $this->resetAfterTest();
         $gitfilelink = \mod_jupyter\git_generator::gen_gitfilelink("", "", "");
         $this->assertEquals($gitfilelink, "/blob//");
     }
+
+    public function test_gen_gitfile_numbers() {
+        $this->resetAfterTest();
+        $gitfilelink = \mod_jupyter\git_generator::gen_gitfilelink(1, 2, 3);
+        $this->assertEquals($gitfilelink, "1/blob/3/2");
+    }
+
+
     public function test_gen_gitpath_standard() {
+        $this->resetAfterTest();
         $gitpath = \mod_jupyter\git_generator::gen_gitpath("https://github.com/username/reponame", "notebook.ipynb", "branch");
-        $this->assertEquals($gitpath, '/hub/user-redirect/git-pull?repo=https%3A%2F%2Fgithub.com%2Fusername%2Freponame&urlpath=lab%2Ftree%2Freponame%2Fnotebook.ipynb&branch=branch');
+        $expected = '/hub/user-redirect/'.
+        'git-pull?repo=https%3A%2F%2Fgithub.com' . '
+        %2Fusername%2Freponame&urlpath=lab%2Ftree%2Freponame%2Fnotebook.ipynb&branch=branch';
+        $expected = trim(preg_replace('/\s+/', '', $expected));
+        $this->assertEquals($gitpath, $expected);
+    }
+
+    public function test_gen_gitpath_empty() {
+        $this->resetAfterTest();
+        $gitpath = \mod_jupyter\git_generator::gen_gitpath("", "", "");
+        $this->assertEquals($gitpath, '/hub/user-redirect/git-pull?repo=&urlpath=lab%2Ftree%2F%2F&branch=');
     }
 }
