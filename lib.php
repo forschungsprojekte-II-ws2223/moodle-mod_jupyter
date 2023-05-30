8c444f12b29b1185b962a3dd44b76c85acf2d547
Maximilian Schüle <max.schuele@gmail.com>
2023-05-30 11:33:40 +0000

    fix comment

--- lib.php
+++ lib.php
@@ -187,7 +187,7 @@ function jupyter_update_grades($moduleinstance, $userid = 0, $nullifnone = true)
     require_once($CFG->libdir.'/gradelib.php');
 
     // Populate array of grade objects indexed by userid.
-    // TODO:
+    // TODO:.
     $grades = array();
     grade_update('/mod/jupyter', $moduleinstance->course, 'mod', 'jupyter', $moduleinstance->id, 0, $grades);
 }
